dcaf1b0dde88a02aaca45dba68816ba86aab972a
Ari-Vincent <ivincentl@foxmail.com>
2022-08-30 23:17:01 +0800

    修复了历史记录无法点击的bug
    原来是app.json没改

--- utils/util.js
+++ utils/util.js
@@ -15,12 +15,12 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
-const json2Form = function(json) {//用于处理post请求的，post请求在微信端有谜之问题
+const json2Form = function (json) { //用于处理post请求的，post请求在微信端有谜之问题
   //参见：微信小程序开发之网络请求(POST请求)
   //链接：https://blog.csdn.net/qq_31383345/article/details/52839482
   var str = [];
-  for(var p in json){
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(json[p]));
+  for (var p in json) {
+    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(json[p]));
   }
   return str.join("&");
 }
@@ -31,19 +31,19 @@ const formatDate = function (times) {
   var month = date.getMonth() + 1; //月份
   var day = date.getDate(); //日
   var hour = function () { //获取小时
-      return date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+    return date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
   }
   var minute = function () { //获取分钟
-      return date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    return date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
   }
 
   var second = function () { //获取秒数
-      return date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+    return date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
   }
   return year + '-' + month + '-' + day + ' ' + hour() + ':' + minute() + ':' + second()
 
 }
-const promisic = function (func) {//供Http类调用
+const promisic = function (func) { //供Http类调用
   return function (params = {}) {
     return new Promise((resolve, reject) => {
       const args = Object.assign(params, {
@@ -58,65 +58,64 @@ const promisic = function (func) {//供Http类调用
     });
   };
 };
-const baseToImgOne=function(obj,name){
+const baseToImgOne = function (obj, name) {
   var base64 = "data:image/jpg;base64," + obj[name];
-   var imgPath = wx.env.USER_DATA_PATH + '/e-invoice' + Date.parse(new Date())+Math.random() + '.jpg';
-   var imageData = base64.replace(/^data:image\/\w+;base64,/, "");
-   var fs = wx.getFileSystemManager();
-   fs.writeFileSync(imgPath, imageData, "base64");
-   fs.close();
-   obj[i].photo=imgPath
-   console.log(imgPath)
+  var imgPath = wx.env.USER_DATA_PATH + '/e-invoice' + Date.parse(new Date()) + Math.random() + '.jpg';
+  var imageData = base64.replace(/^data:image\/\w+;base64,/, "");
+  var fs = wx.getFileSystemManager();
+  fs.writeFileSync(imgPath, imageData, "base64");
+  fs.close();
+  obj[i].photo = imgPath
+  console.log(imgPath)
 }
-const base64toImg=function(obj){
+const base64toImg = function (obj) {
   console.log(obj.length)
-for(var i=0,len= obj.length;i<len;i++){
+  for (var i = 0, len = obj.length; i < len; i++) {
 
-  if(obj[i].hasOwnProperty('photo'))
-  if(obj[i].photo)
-   { var base64 = "data:image/jpg;base64," + obj[i].photo;
-   var imgPath = wx.env.USER_DATA_PATH + '/e-invoice' + Date.parse(new Date())+Math.random() + '.jpg';
-   var imageData = base64.replace(/^data:image\/\w+;base64,/, "");
-   var fs = wx.getFileSystemManager();
-   fs.writeFileSync(imgPath, imageData, "base64");
-   fs.close();
-   obj[i].photo=imgPath
-   console.log(imgPath)
-}
- ;
- if(obj[i].hasOwnProperty('prove'))
- if(obj[i].prove)
-  obj[i].prove="data:image/jpeg;base64,"+obj[i].prove.
-  replace(/[\r\n]/g, '');
-;
-if(obj[i].hasOwnProperty('nucleicAcidProof'))
-  if(obj[i].nucleicAcidProof)
-   obj[i].nucleicAcidProof="data:image/jpeg;base64,"+obj[i].nucleicAcidProof.
-   replace(/[\r\n]/g, '');
- ;
- if(obj[i].hasOwnProperty('healthCode'))
-  if(obj[i].healthCode)
-   obj[i].healthCode="data:image/jpeg;base64,"+obj[i].healthCode.
-   replace(/[\r\n]/g, '');
- ;
+    if (obj[i].hasOwnProperty('photo'))
+      if (obj[i].photo) {
+        var base64 = "data:image/jpg;base64," + obj[i].photo;
+        var imgPath = wx.env.USER_DATA_PATH + '/e-invoice' + Date.parse(new Date()) + Math.random() + '.jpg';
+        var imageData = base64.replace(/^data:image\/\w+;base64,/, "");
+        var fs = wx.getFileSystemManager();
+        fs.writeFileSync(imgPath, imageData, "base64");
+        fs.close();
+        obj[i].photo = imgPath
+        console.log(imgPath)
+      };
+    if (obj[i].hasOwnProperty('prove'))
+      if (obj[i].prove)
+        obj[i].prove = "data:image/jpeg;base64," + obj[i].prove.replace(/[\r\n]/g, '')
+        ;
+        ;
+    if (obj[i].hasOwnProperty('nucleicAcidProof'))
+      if (obj[i].nucleicAcidProof)
+        obj[i].nucleicAcidProof = "data:image/jpeg;base64," + obj[i].nucleicAcidProof.replace(/[\r\n]/g, '')
+        ;
+      ;
+    if (obj[i].hasOwnProperty('healthCode'))
+      if (obj[i].healthCode)
+        obj[i].healthCode = "data:image/jpeg;base64," + obj[i].healthCode.replace(/[\r\n]/g, '')
+      ;
+    ;
 
-}
+  }
 
 
 
 }
 
 class Http {
-    // 同步Http请求
-    static async asyncRequest(url, method, data, header,backMethod) {//供页面中的同步请求调用
-        let res = await promisic(wx.request)({
-            url: url,
-            method: method,
-            data: data,
-            header:header
-        })
-        backMethod(res)
-    }
+  // 同步Http请求
+  static async asyncRequest(url, method, data, header, backMethod) { //供页面中的同步请求调用
+    let res = await promisic(wx.request)({
+      url: url,
+      method: method,
+      data: data,
+      header: header
+    })
+    backMethod(res)
+  }
 }
 
 //向外暴露,供页面调用
@@ -128,4 +127,6 @@ export default {
   base64toImg
 }
 
-export {Http}
+export {
+  Http
+}
